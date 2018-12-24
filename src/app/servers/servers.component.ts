346055a0d7557ebcdbdb82f1e1daa7a07ecebaf3
Kishan Patel <kolipatelkishan786@gmail.com>
2018-12-24 17:22:28 +0530

    Using ngif to output data conditionally.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -14,7 +14,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No Server was Creation!';
   serverName = 'Test server';
-
+  servercreated = false;
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -25,7 +25,9 @@ export class ServersComponent implements OnInit {
   }
 
   onCreatetionServer() {
-    this.serverCreationStatus = 'server was Created!';
+    this.servercreated = true;
+    this.serverCreationStatus = 'server was Created! ' + this.serverName;
+
   }
   onUpdateserverName(event: Event) {
       this.serverName = (<HTMLInputElement>event.target).value;
