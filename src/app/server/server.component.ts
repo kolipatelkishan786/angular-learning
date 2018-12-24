f913c6f0b258b9e439d4268c480b7eaf9bedcf63
Kishan Patel <kolipatelkishan786@gmail.com>
2018-12-24 17:56:09 +0530

    style element daynamicallywith ngstyle.

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,11 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class ServerComponent implements OnInit {
   serverid: number = 10;
   serverstatus: string = 'offline';
-  constructor() { }
+  constructor() {
+    this.serverstatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
 
   ngOnInit() {
   }
   getserverstatus() {
     return this.serverstatus;
+  }
+  getcolor() {
+    return this.serverstatus === 'online' ? 'green' : 'red';
   }
 }
